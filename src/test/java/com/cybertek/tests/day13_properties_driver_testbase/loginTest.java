ffddc4e833f40a97e0ec501d4bec811d738a19b5
Musa Meral <meralmoussa@icloud.com>
2019-12-12 22:55:46 +0100

    New Commit

--- src/test/java/com/cybertek/tests/day13_properties_driver_testbase/loginTest.java
+++ src/test/java/com/cybertek/tests/day13_properties_driver_testbase/loginTest.java
@@ -1,5 +1,6 @@
 package com.cybertek.tests.day13_properties_driver_testbase;
 
+import com.cybertek.tests.TestBase;
 import com.cybertek.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -8,19 +9,10 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class loginTest {
+public class loginTest extends TestBase {
 
 
-    WebDriver driver;
-    @BeforeMethod
-    public void setUpMethod(){
-        driver = Driver.get();
-    }
-    @AfterMethod
-    public void tearDownMethod() throws InterruptedException {
-        Thread.sleep(2000);
-        driver.quit();
-    }
+
 
     @Test
     public void test1(){
