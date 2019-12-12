8e9a5fe45e6954d89b061c7136557e97291c9684
Musa Meral <meralmoussa@icloud.com>
2019-12-12 21:49:57 +0100

    New Commit

---
+++ src/test/java/com/cybertek/tests/day13_properties_driver_testbase/PropertiesTest.java
@@ -0,0 +1,25 @@
+package com.cybertek.tests.day13_properties_driver_testbase;
+
+import com.cybertek.utulities.ConfigurationReader;
+import org.testng.annotations.Test;
+
+public class PropertiesTest {
+
+    @Test
+    public void test1(){
+
+        String browser = ConfigurationReader.get("browser");
+        System.out.println(ConfigurationReader.get("url"));
+
+        System.out.println(ConfigurationReader.get("salesmanager_username"));
+        System.out.println(ConfigurationReader.get("storemanager"));
+
+
+
+
+
+
+    }
+
+
+}
